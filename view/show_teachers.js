48b1bdef2b2700e54f255e546cd3480bf952bdd2
ali <ali79taba@gmail.com>
2020-11-28 00:22:40 +0330

    for chosen teacher add button

--- view/show_teachers.js
+++ view/show_teachers.js
@@ -10,9 +10,9 @@ const main_view = require('./student_main_page');
 
 const Teacher = require('../models/teacher');
 
-exports.whichWant = (msg) => {
+function whichWant (msg, teacherId) {
     const chatId = msg.chat.id;
-    let teacherId = fix_number(msg.text);
+    teacherId = fix_number(teacherId);
     Teacher.findOne({where: {id: teacherId}}).then(async teacher => {
         if (teacher) {
             // await show_one_teacher(chatId, teacher).catch(err => {
@@ -35,32 +35,44 @@ exports.whichWant = (msg) => {
             //     });
             msg.text = "1";
             await show_teachers_controloer.controlHandler(msg, teacherId);
-        }else{
+        } else {
             functionHadler.updateState(chatId, "");
             bot.bot.sendMessage(chatId, "کد وارد شده غلط می باشد");
             main_view.show_list(chatId);
         }
     });
 
-};
+}
+
+exports.whichWant = whichWant;
 
 async function show_one_teacher(chatId, teacher) {
+    const inline_keyboard =[[{
+        text: "انتخاب استاد " + teacher.first_name + " " + teacher.last_name ,
+        callback_data: teacher.id,
+    }]];
     let pic_stream;
     let caption = "نام استاد : " + teacher.first_name + " " + teacher.last_name + "\n";
-    if(teacher.description){
+    if (teacher.description) {
         caption += teacher.description + "\n";
     }
-    caption +=  "کد استاد : " + teacher.id;
+    caption += "کد استاد : " + teacher.id;
     let show_pic = true;
     // pic_stream = request.get(teacher.image_link).on('error', function(err) { show_pic = false});
     pic_stream = await request.get(teacher.image_link, function (error, response, body) {
         if (error)
             show_pic = false;
     });
+    const options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: inline_keyboard
+        }),
+        caption: caption
+    };
     if (!show_pic) {
         await bot.bot.sendMessage(chatId, caption);
     } else {
-        await bot.bot.sendPhoto(chatId, pic_stream, {caption: caption});
+        await bot.bot.sendPhoto(chatId, pic_stream, options);
     }
 }
 
@@ -74,19 +86,27 @@ exports.show_teachers = async (chatId, teachers) => {
         await bot.bot.sendMessage(chatId, response);
         functionHadler.updateState(chatId, '');
     } else {
+        bot.bot.once('callback_query', (msg) => {
+            const message = msg.message;
+            try{
+                whichWant(message, msg.data);
+            }catch (e) {
+                console.log("OK", e);
+            }
+        })
         let caption = "";
         response = "برای درخواست مشاوره از استاد کد استاد مورد نظر خود را انتخاب کنید\n";
-        functionHadler.updateState(chatId,'get_teacher_code');
+        functionHadler.updateState(chatId, '');
         for (const i in teachers) {
-            await show_one_teacher(chatId, teachers[i]).catch(err=>{
-                console.log("ERR WHILE SHOW TEACHER PIC");
+            await show_one_teacher(chatId, teachers[i]).catch(err => {
+                console.log("ERR WHILE SHOW TEACHER PIC : ", err);
                 console.log("teacherId : ", i);
             });
             // const teacher = teachers[i];
             // caption += "نام استاد : " + teacher.first_name + " " + teacher.last_name + "\n" + "کد استاد : " + teacher.id + "\n------\n";
         }
         // await bot.bot.sendMessage(chatId, caption);
-        await bot.bot.sendMessage(chatId, response)
+        // await bot.bot.sendMessage(chatId, response)
         // await bot.bot
         //     .sendMessage(chatId, response, {
         //         reply_markup: JSON.stringify({force_reply: true})
